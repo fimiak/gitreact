960e40b4cc9b4cdbd902e5eedd0447369308f39b
fimiak <fimiak@gmail.com>
2015-07-02 11:46:00 -0700

    React Github

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,16 +1,19 @@
 var React = require('react');
+var NotesList = require('./NotesList');
+var AddNote = require('./AddNote');
 
 var Notes = React.createClass({
     propTypes: {
         username: React.PropTypes.string.isRequired,
-        notes: React.PropTypes.array.isRequired
+        notes: React.PropTypes.array.isRequired,
+        addNote: React.PropTypes.func.isRequired
     },
     render: function () {
         return (
             <div>
-                NOTES <br />
-                Username: {this.props.username} <br />
-                Notes: {this.props.notes}
+                <h3>Note for {this.props.username} </h3>
+                <AddNote username={this.props.username} addNote={this.props.addNote} />
+                <NotesList notes={this.props.notes} />
             </div>
         )
     }
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -4,23 +4,43 @@ var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
+var helpers = require('../utils/helpers');
 
 var Profile = React.createClass({
     mixins: [Router.State, ReactFireMixin],
     getInitialState: function() {
         return {
-            notes: ['note1', 'note2'],
-            bio: {name: "Name"},
-            repos: [1,2,3]
+            notes: [],
+            bio: {},
+            repos: []
         }
     },
-    componentDidMount: function() {
-        this.ref = new Firebase('https://torrid-inferno-143.firebaseio.com/');
+    init: function () {
         var childRef = this.ref.child(this.getParams().username);
         this.bindAsArray(childRef, 'notes');
+
+        helpers.getGithubInfo(this.getParams().username)
+            .then(function (dataObj) {
+                this.setState({
+                    bio: dataObj.bio,
+                    repos: dataObj.repos
+                });
+            }.bind(this));
+    },
+    componentDidMount: function() {
+        this.ref = new Firebase('https://torrid-inferno-143.firebaseio.com');
+        this.init();
     },
     componentWillUnmount: function () {
         this.unbind('notes')
+    },
+    componentWillReceiveProps: function () {
+        this.unbind('notes');
+        this.init();
+    },
+    handleAddNote: function (newNote) {
+        this.ref.child(this.getParams().username).set(this.state.notes.concat([newNote]));
     },
     render: function () {
         var username = this.getParams().username;
@@ -33,7 +53,10 @@ var Profile = React.createClass({
                     <Repos username={username} repos={this.state.repos} />
                 </div>
                 <div className="col-md-4">
-                    <Notes username={username} notes={this.state.notes} />
+                    <Notes
+                        username={username}
+                        notes={this.state.notes}
+                        addNote={this.handleAddNote} />
                 </div>
             </div>
         )
